054c59f178fe7e67dbcb5cd83b5cb7cd9d0bc102
RealAtix <lana20razvan@gmail.com>
2016-08-29 12:24:40 +0200

    Add update notification

--- src/view/Launcher.java
+++ src/view/Launcher.java
@@ -3,9 +3,14 @@ package view;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import javax.swing.JOptionPane;
+
+import org.jsoup.Jsoup;
+
 import controller.WorldController;
 
 public class Launcher {
+	
+	private final static String VERSION = "v1.2";
 
 	public static void main(String[] args) {
 		WorldController w = null;
@@ -13,13 +18,21 @@ public class Launcher {
 			w = new WorldController();
 			w.showUI();
 			w.updateWorlds();
+			if (updateAvailable() == true) { JOptionPane.showMessageDialog(null, "A new version has been released, you can find the newest version at\nhttps://github.com/RealAtix/OSRS-World-Tracker", "Update", JOptionPane.WARNING_MESSAGE); }
 		} catch (UnknownHostException e) {
 			JOptionPane.showMessageDialog(null, "Can't reach oldschool.runescape.com, check your internet connection", "Host unreachable", JOptionPane.ERROR_MESSAGE);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (NullPointerException e) {
-			JOptionPane.showMessageDialog(null, "Jaunt API has expired, please update OSRS World Tracker to the newest version\n\nhttps://github.com/RealAtix/OSRS-World-Tracker", "Out of date", JOptionPane.ERROR_MESSAGE);
 		}
+	}
+	
+	private static Boolean updateAvailable() throws IOException {
+		String json = Jsoup.connect("https://api.github.com/repos/RealAtix/OSRS-World-Tracker/releases/latest").ignoreContentType(true).execute().body();
+		String version = json.substring(json.indexOf("tag_name") + 11, json.indexOf("target_commitish") - 3);
+		if (!VERSION.equals(version)) {
+			return true;
+		}
+		return false;
 	}
 
 }
